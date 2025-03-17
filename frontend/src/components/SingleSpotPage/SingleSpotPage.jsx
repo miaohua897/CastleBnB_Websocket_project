e8aa168c4306f1e72f43cc8e5d3acfd9a319fe93
miaohua897 <haijianhoubj@gmail.com>
2025-03-17 15:57:37 -0700

    singleSpotPage css

--- frontend/src/components/SingleSpotPage/SingleSpotPage.jsx
+++ frontend/src/components/SingleSpotPage/SingleSpotPage.jsx
@@ -1,66 +1,48 @@
 import { useSelector } from 'react-redux';
 import { FaStar } from 'react-icons/fa6'; 
 import { useNavigate } from 'react-router-dom';
-
 import './SingleSpotPage.css';
-
-
-
 
 function SingleSpotPage(){
     const navigate = useNavigate();   
     const spots = useSelector(state=>
         state.spot.Spots
     );
-
     const handleimage=(value)=>{
         if(!value) return "https://res.cloudinary.com/dsgfqkf7n/image/upload/eight_mezc8g?_a=BAMCkGfi0";
         else return value.url;
     }
-  
-
     return (
-    <div className='SpotsContainer'>
-       
+    <div className='spots-container'>    
      {
      spots?   
      spots.map(spot=>{
         return (
-          <div className='SingleSpotletout' key={spot.id}  onClick={()=>{
-            navigate(`/spots/${spot.id}`);
-          }}>
-        <div className="tooltip-container">
-        <img 
-        style={{width:270,height:130}}
-          src={handleimage(spot.previewImage)} 
-          />
-           <span className='tooltiptext'>{spot.name}</span>
-        </div>
-           <ul className='SpotInfomation'>
-            <li>{spot.city}{",  "+spot.state}</li>
-            <li >
-             <a style={{
-          color: spot.avgRating? 'black': 'lightgray' ,  
-          transition: 'color 0.05s ease',
-          cursor: 'pointer'  
-        }}> <FaStar /></a> 
-            {'    '}   
-             {spot.avgRating?
-           
-            (Math.round(spot.avgRating * 10) / 10===5||Math.round(spot.avgRating * 10) / 10===4||Math.round(spot.avgRating * 10) / 10===3||Math.round(spot.avgRating * 10) / 10===2||Math.round(spot.avgRating * 10) / 10===1? `${Math.round(spot.avgRating * 10) / 10}.0`:Math.round(spot.avgRating * 10) / 10)
-  
-            :"New"} 
-            </li>
-        </ul>
-    
-        <p>{"$"+spot.price+"  night"}</p> 
-
+          <div className='single-spot-outlet' key={spot.id}  onClick={()=>{navigate(`/spots/${spot.id}`);}}>
+            <div className="tooltip-container">
+                <img style={{width:260,height:150}}
+                  src={handleimage(spot.previewImage)}  />
+                <span className='tooltiptext'>{spot.name}</span>
+            </div>
+            <div className='spot-information-container'>
+                <ul className='spot-information'>
+                    <li>{spot.city}{",  "+spot.state}</li>
+                    <li >
+                      <a style={{
+                        color: spot.avgRating? 'black': 'lightgray' ,  
+                        transition: 'color 0.05s ease',
+                        cursor: 'pointer'  
+                        }}> <FaStar /></a> 
+                        {'    '}   
+                        {spot.avgRating?
+                        (Math.round(spot.avgRating * 10) / 10===5||Math.round(spot.avgRating * 10) / 10===4||Math.round(spot.avgRating * 10) / 10===3||Math.round(spot.avgRating * 10) / 10===2||Math.round(spot.avgRating * 10) / 10===1? `${Math.round(spot.avgRating * 10) / 10}.0`:Math.round(spot.avgRating * 10) / 10)
+                        :"New"} 
+                    </li>
+                </ul>
+                  <p>{"$"+spot.price+"  night"}</p>
+            </div>
           </div>
-        )    
-   }):null
- 
-     }
-  
+        )}):null}
     </div>)
 }
 export default SingleSpotPage
